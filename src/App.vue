f8328b13f20b39bb3399a11b45dd0c4a7caac4e3
Nghi Nguyen <ndnghi.2000@gmail.com>
2023-04-05 19:34:29 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import { computed, onMounted, ref, watch } from "vue";
+import { computed, onMounted, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useStore } from "vuex";
 
